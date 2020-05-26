754d15e1b0536f8cdfa53d8c3367edfba544e2b6
nathanjonjon <b05902112@ntu.edu.tw>
2020-05-26 11:06:47 +0800

    go

--- merge.sh
+++ merge.sh
@@ -35,8 +35,9 @@ if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
     # Merge the dev into latest stable
     # echo "Merging changes..." && \
     # git merge $FROM_BRANCH && \
+    git branch --set-upstream-to=origin/$TO_BRANCH TO_BRANCH
     git pull
-    # touch hello
+    touch hello
     # git pull --rebase origin $FROM_BRANCH
     ls -a
 
