fed22c173e7b94fb7be5025e50fe5e46c6fb810c
nathanjonjon <b05902112@ntu.edu.tw>
2020-05-26 10:57:35 +0800

    try

--- merge.sh
+++ merge.sh
@@ -35,8 +35,8 @@ if [ "$CURRENT_BRANCH" = "$FROM_BRANCH" ] ; then
     # Merge the dev into latest stable
     # echo "Merging changes..." && \
     # git merge $FROM_BRANCH && \
-    git pull --rebase origin $TO_BRANCH
-    git pull --rebase origin $FROM_BRANCH
+    git pull $TO_BRANCH
+    # git pull --rebase origin $FROM_BRANCH
     ls -a
 
     git add .
